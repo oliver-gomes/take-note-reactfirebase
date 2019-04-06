a52c02b2cc62ad97fb2feae08a8cb88a3894f0fb
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-06 18:37:11 -0400

    import Notes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
-import * as firebase from "firebase";
+
 import Header from "./components/Header";
 import NotesForm from "./components/NotesForm";
+import Notes from "./components/Notes";
 
 class App extends Component {
+  state = {
+    notes: []
+  };
+
+  
   render() {
     return (
       <div className="App">
         <Header />
         <main>
           <NotesForm />
+          <Notes notes={this.state.notes} />
         </main>
       </div>
     );
