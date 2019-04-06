d07df9de6ea59efa8e28bbff9403f9e76eb178fa
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-06 18:37:04 -0400

    Notes created

---
+++ src/components/Notes.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Notes extends Component {
+  render() {
+    return (
+      <section className="notes-wrapper">
+        <h3>Notes</h3>
+        <div className="notes" />
+      </section>
+    );
+  }
+}
