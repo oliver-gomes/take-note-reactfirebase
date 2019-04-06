ecb9a3171b0d51424d95dc113b32a14871a5ca38
Oliver Gomes <ogomesphoto@gmail.com>
2019-04-06 18:08:35 -0400

    changeHandler implement

--- src/components/NotesForm.js
+++ src/components/NotesForm.js
@@ -6,17 +6,35 @@ export default class NotesForm extends Component {
     title: "",
     note: ""
   };
+
+  onChangeHandler = (e, key) => {
+    this.setState({
+      [key]: e.target.value
+    });
+  };
+
   render() {
     return (
       <section className="noteform">
         <h3>Create New Note</h3>
         <div className="form-group">
           <label htmlFor="noteform-title">Title</label>
-          <input type="text" className="noteform-title" id="noteform-title" />
+          <input
+            type="text"
+            className="noteform-title"
+            id="noteform-title"
+            value={this.state.title}
+            onChange={e => this.onChangeHandler(e, "title")}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="noteform-note">Note</label>
-          <textarea name="noteform-note" id="noteform-note" />
+          <textarea
+            name="noteform-note"
+            id="noteform-note"
+            value={this.state.note}
+            onChange={e => this.onChangeHandler(e, "note")}
+          />
         </div>
         <button>Create Note</button>
       </section>
